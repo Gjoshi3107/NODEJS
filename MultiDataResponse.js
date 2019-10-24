34bbe30d6b96f402e92079c710786848c0e21048
Gaurav <Gjoshi3107@gmail.com>
2019-10-24 17:17:24 +0530

    updates

--- MultiDataResponse.js
+++ MultiDataResponse.js
@@ -12,26 +12,26 @@ var server = http.createServer(function (req, res) {
         // .jpg = 'image/jpeg'
         // .jpeg = 'image/jpeg'
 
-        res.writeHead(200, {'Content-Type': 'image/x-icon'});              // for JSON:- application/json; html:- text/html
-        fs.createReadStream(__dirname+'/favicon.ico').pipe(res);                   // a readStream is created and the output is piped to a writeStream.
+        res.writeHead(200, { 'Content-Type': 'image/x-icon' });              // for JSON:- application/json; html:- text/html
+        fs.createReadStream(__dirname + '/favicon.ico').pipe(res);                   // a readStream is created and the output is piped to a writeStream.
     }
     else if (req.method === 'GET' && req.url === "/" || req.url === "/home") {
-        res.writeHead(200, {'Content-Type': "text/html" });
+        res.writeHead(200, { 'Content-Type': "text/html" });
         fs.createReadStream(__dirname + "/home.html", "utf-8").pipe(res);
     }
     else if (req.method === 'GET' && req.url === '/contact') {
-        res.writeHead(200, {'Content-Type': "text/html" });
+        res.writeHead(200, { 'Content-Type': "text/html" });
         fs.createReadStream(__dirname + "/contact.html", "utf-8").pipe(res);
     }
     else if (req.method === 'GET' && req.url === '/json') {
-        res.writeHead(200, {'Content-Type': "application/json" });
+        res.writeHead(200, { 'Content-Type': "application/json" });
         fs.createReadStream(__dirname + "/contactJSON.json").pipe(res);
     }
     else {
-        res.writeHead(404, {'Content-Type': "text/html" });
+        res.writeHead(404, { 'Content-Type': "text/html" });
         fs.createReadStream(__dirname + "/failure.html", "utf-8").pipe(res);
     }
 });
 
-server.listen(3000,"127.0.0.1");
+server.listen(3000, "127.0.0.1");
 console.log("Listening to port 3000 of IP 127.0.0.1");
